9780ecec75f03ebe10d909d96233ac48b7883af1
Spiros Tzavellas <spiros@tzavellas.com>
2010-08-16 15:34:46 +0300

    Docs and tests for the AnnotationNamingStrategy.

--- src/main/scala/com/tzavellas/sse/jmx/export/AnnotationNamingStrategy.scala
+++ src/main/scala/com/tzavellas/sse/jmx/export/AnnotationNamingStrategy.scala
@@ -2,8 +2,16 @@ package com.tzavellas.sse.jmx.export
 
 import javax.management.ObjectName
 
-object AnnotationNamingStrategy extends ObjectNamingStrategy {
+/**
+ * Derive an object name from the {@code objectName} value of the
+ * {@literal @ManagedResource} annotation.
+ */
+private object AnnotationNamingStrategy extends ObjectNamingStrategy {
   
+  /**
+   * Supports only classes that contains a non-empty string in the
+   * {@code objectName} value of a {@literal @ManagedResource} annotation.
+   */
   def canCreateNameFor(clazz: Class[_]) = { 
     if (clazz.isAnnotationPresent(classOf[ManagedResource]))
       clazz.getAnnotation(classOf[ManagedResource]).objectName != ""
@@ -11,10 +19,14 @@ object AnnotationNamingStrategy extends ObjectNamingStrategy {
       false
   }
   
-  def nameFor(clazz: Class[_], keyProperties: Map[String, Any]): ObjectName = {
+  /**
+   * Create an {@code ObjectName} using the {@code objectName} value
+   * of the {@literal @ManagedResource} annotation that is present in
+   * the specified class.
+   */
+  def nameFor(clazz: Class[_]): ObjectName = {
     val managedResource = clazz.getAnnotation(classOf[ManagedResource])
     assert(managedResource != null)
-    
     new ObjectName(managedResource.objectName)
   }
 }
