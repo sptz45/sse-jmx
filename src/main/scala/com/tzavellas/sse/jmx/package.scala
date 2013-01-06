103148bd61142555da23fe68fc8e707dcedb2779
Spiros Tzavellas <spiros@tzavellas.com>
2013-01-06 18:15:51 +0200

    Add package object (for scaladoc).

---
+++ src/main/scala/com/tzavellas/sse/jmx/package.scala
@@ -0,0 +1,10 @@
+/* ------------------- sse-jmx ------------------- *\
+ * Licensed under the Apache License, Version 2.0. *
+ * Author: Spiros Tzavellas                        *
+\* ----------------------------------------------- */
+package com.tzavellas.sse
+
+/**
+ * Provides classes for using JMX in Scala.
+ */
+package object jmx { }
