7b17b12ef5b39a7ce61da25fe5bfa4337fe7b84e
Spiros Tzavellas <spiros@tzavellas.com>
2013-01-07 21:57:54 +0200

    Add more tests to JmxUtils.

--- src/test/scala/com/tzavellas/sse/jmx/JmxUtilsTest.scala
+++ src/test/scala/com/tzavellas/sse/jmx/JmxUtilsTest.scala
@@ -13,6 +13,16 @@ class JmxUtilsTest {
   def should_detect_an_mbean() {
     assertTrue(JmxUtils.isStandardMBean(classOf[Helper]))
     assertFalse(JmxUtils.isStandardMBean(classOf[String]))
+    
+    assertTrue(JmxUtils.isStandardMBeanInterface(classOf[HelperMBean]))
+  }
+  
+  @Test
+  def should_detect_an_standard_mbean_interface() {
+    assertTrue(JmxUtils.isStandardMBeanInterface(classOf[HelperMBean]))
+    assertFalse(JmxUtils.isStandardMBeanInterface(classOf[HelperMXBean]))
+    assertFalse(JmxUtils.isStandardMBeanInterface(classOf[ClassMBean]))
+    assertFalse(JmxUtils.isStandardMBeanInterface(classOf[Helper])) // not interface
   }
   
   @Test
@@ -21,7 +31,18 @@ class JmxUtilsTest {
     assertFalse(JmxUtils.isMXBean(classOf[String]))
   }
   
+  @Test
+  def should_detect_an_standard_mxbean_interface() {
+    assertTrue(JmxUtils.isMXBeanInterface(classOf[HelperMXBean]))
+    assertFalse(JmxUtils.isMXBeanInterface(classOf[HelperMBean]))
+    assertFalse(JmxUtils.isMXBeanInterface(classOf[ClassMXBean]))
+    assertFalse(JmxUtils.isMXBeanInterface(classOf[Helper])) // not interface
+  }
+  
   trait HelperMBean
   trait HelperMXBean
   class Helper extends HelperMBean with HelperMXBean
+  
+  class ClassMBean
+  class ClassMXBean
 }
