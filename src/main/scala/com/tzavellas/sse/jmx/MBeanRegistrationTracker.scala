420bfb00bc3bf4a86cecf8a3611d1f77ae483262
Spiros Tzavellas <spiros@tzavellas.com>
2014-11-17 11:57:40 +0200

    Remove deprecation warning for synchronized collection.

--- src/main/scala/com/tzavellas/sse/jmx/MBeanRegistrationTracker.scala
+++ src/main/scala/com/tzavellas/sse/jmx/MBeanRegistrationTracker.scala
@@ -5,25 +5,26 @@
 package com.tzavellas.sse.jmx
 
 import scala.collection.mutable.{HashSet, SynchronizedSet}
+import java.util.concurrent.ConcurrentHashMap
 import javax.management.{ObjectName, InstanceNotFoundException}
 
 /**
  * Tracks MBean registrations.
- * 
+ *
  * @see [[unregisterAll]]
  */
 trait MBeanRegistrationTracker extends MBeanRegistrationSupport {
 
-  private val registered = new HashSet[ObjectName] with SynchronizedSet[ObjectName]
+  private val registered = new ConcurrentHashMap[ObjectName, Unit]
 
   abstract override def registerMBean(mbean: AnyRef, name: ObjectName, behavior: IfAlreadyExists.Enum = IfAlreadyExists.Fail) {
     super.registerMBean(mbean, name, behavior)
-    registered += name
+    registered.putIfAbsent(name, ())
   }
 
   abstract override def unregisterMBean(name: ObjectName, ignore: Boolean = false) {
     super.unregisterMBean(name)
-    registered -= name
+    registered.remove(name)
   }
 
   /**
@@ -32,9 +33,14 @@ trait MBeanRegistrationTracker extends MBeanRegistrationSupport {
    * @see [[registerMBean]]
    */
   def unregisterAll() {
-    for (name <- registered) {
-      try { unregisterMBean(name) }
-      catch { case e: InstanceNotFoundException => }
+    val names = registered.keySet.iterator
+    while(names.hasNext) {
+      try {
+        unregisterMBean(names.next)
+      } catch {
+        case e: InstanceNotFoundException =>
+      }
+      names.remove()
     }
   }
 }
